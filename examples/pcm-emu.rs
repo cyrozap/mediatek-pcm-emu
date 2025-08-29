fefc6842e4be43e610614dd77fbfbdd87ffac4a3
cyrozap <cyrozap@gmail.com>
2025-08-29 13:13:41 -0500

    Update copyright year
    Fixes: 6019626 ("Update Rust edition to 2024")

--- examples/pcm-emu.rs
+++ examples/pcm-emu.rs
@@ -1,6 +1,6 @@
 /*
  *  pcm-emu.rs - A basic emulator for the MediaTek PCM.
- *  Copyright (C) 2022-2023  Forest Crossman <cyrozap@gmail.com>
+ *  Copyright (C) 2022-2023, 2025  Forest Crossman <cyrozap@gmail.com>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
